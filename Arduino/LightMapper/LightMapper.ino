b4dff7666c623bcc6f48767fed75b6cd1c01529f
MicahW <micah.wallberg@gmail.com>
2020-03-06 17:22:24 -0500

    Added motor states

--- Arduino/LightMapper/LightMapper.ino
+++ Arduino/LightMapper/LightMapper.ino
@@ -1,10 +1,32 @@
+#include <stdint.h>
 #include <SoftwareSerial.h>
+
+#define MOTOR_STATE_COUNT 8
+
+// A state the motor bridges can be in
+struct MotorState {
+  uint8_t bridge_count : 1;  // 0 for bridge_0 only, 1 for both bridges
+  uint8_t bridge_0 : 2;  // pin offset for the first bridge
+  uint8_t bridge_1 : 2;  // pin offset for the second bridge
+};
+
+const MotorState motor_states[MOTOR_STATE_COUNT] {
+  MotorState {.bridge_count = 1, .bridge_0 = 0,},
+  MotorState {.bridge_count = 2, .bridge_0 = 0, .bridge_1 = 2},
+  MotorState {.bridge_count = 1, .bridge_0 = 2,},
+  MotorState {.bridge_count = 2, .bridge_0 = 1, .bridge_1 = 2},
+  MotorState {.bridge_count = 1, .bridge_0 = 1,},
+  MotorState {.bridge_count = 2, .bridge_0 = 1, .bridge_1 = 3},
+  MotorState {.bridge_count = 1, .bridge_0 = 3,},
+  MotorState {.bridge_count = 2, .bridge_0 = 0, .bridge_1 = 3}
+};
+
 SoftwareSerial BTserial(2, 3); // RX | TX
  
 void setup() 
 {
 #ifdef DEBUG
-Serial.begin(9600);
+  Serial.begin(9600);
 #endif
     BTserial.begin(9600);  // Init bluetooth serial connection
 }
