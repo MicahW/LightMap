cbd9ec5f11a6006e9c72ab7d8653edb3c5a3af3f
MicahW <micah.wallberg@gmail.com>
2020-03-07 08:30:57 -0500

    Added setMotorDelayAndDirection

--- Arduino/LightMapper/LightMapper.ino
+++ Arduino/LightMapper/LightMapper.ino
@@ -11,7 +11,12 @@
 #define RIGHT_MOTOR_START_NUMBER  2  // Start index of the left motor
 #define LEFT_MOTOR_START_NUMBER  6  // Start index of the right motor
 
-// A state the motor bridges can be in
+// Motor control mask information
+#define MOTOR_CONTROL_DIRECTION_MASK 0x80
+#define MOTOR_CONTROL_DIRECTION_INDEX 7
+#define MOTOR_CONTROL_VALUE_MASK 0x7F
+
+// A state the motor bridge pins can be in
 struct MotorBridgeState {
   uint8_t bridge_count : 1;  // 0 for bridge_0 only, 1 for both bridges
   uint8_t bridge_0 : 2;  // pin offset for the first bridge
@@ -28,7 +33,7 @@ struct MotorState {
   unsigned long last_step_time;  // Time of last motor step in miliseconds
   unsigned long step_delay;  // Delay in miliseconds between each step (0 indicates no step should be taken)
   uint8_t direction;  // <0 = foward, 1 = backward>
-  uint8_t current_bridge_state_index;  // Current index within the motor_bridge_states
+  uint8_t bridge_state_index;  // Current index within the motor_bridge_states
 };
 
 // Each motor bridge state to cycle between in order to make the motor spin using half steps
@@ -47,10 +52,25 @@ const MotorBridgeState motor_bridge_states[MOTOR_STATE_COUNT] {
 const SoftwareSerial BTserial(2, 3); // RX | TX
  
 // Motor states for each motor
-MotorState motors_states[2] {
+MotorState motor_states[2] {
   MotorState {.reversed = 0, .pin_0_index = RIGHT_MOTOR_START_NUMBER},  // Right motor
   MotorState {.reversed = 1, .pin_0_index = LEFT_MOTOR_START_NUMBER}  // Left motor
 };
+
+// Set the motor delay from a control value provided in a motor control message
+//
+// control_value: The one byte control value from a control message
+// motor_state_index: The index of the motor_state the delay is being set for
+void setMotorDelayAndDirection(uint8_t control_value, uint8_t motor_state_index) {
+  // Get the values from the message
+  uint8_t direction = (control_value & MOTOR_CONTROL_DIRECTION_MASK) >> MOTOR_CONTROL_DIRECTION_INDEX;
+  unsigned long value = (unsigned long) control_value & MOTOR_CONTROL_VALUE_MASK; 
+
+  // Calculate the delay
+  unsigned long delay = MAX_STEP_DELAY - ((value * (MAX_STEP_DELAY - MIN_STEP_DELAY)) / MOTOR_CONTROL_VALUE_MASK);
+  motor_states[motor_state_index].step_delay = delay;
+  motor_states[motor_state_index].direction = direction;
+}
 
 void setup() 
 {
