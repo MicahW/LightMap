6adbbdd459902cf74a4c15f04f5f8cebf8d7bbaa
MicahW <micah.wallberg@gmail.com>
2020-05-31 07:56:18 -0400

    Moved defines to static const vars

--- Arduino/LightMapper/LightMapper.ino
+++ Arduino/LightMapper/LightMapper.ino
@@ -2,55 +2,55 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
-#define MIN_STEP_DELAY 2000  // Minimum delay in microseconds of the motor (highest speed)
-#define MAX_STEP_DELAY 20000 // Maximum delay (lowest speed)
+static const uint32_t kMinStepDelay = 2000;  // Minimum delay in microseconds of the motor (highest speed)
+static const uint32_t kMaxStepDelay = 20000;  // Maximum delay (lowest speed)
 
 // Pin mappings
-#define BLUETOOTH_RX_PIN 12
-#define BLUETOOTH_TX_PIN 13
-#define SERVO_CONTROL_PIN 11
-#define TRIGGER_PIN 1
-#define ECHO_PIN 0
+static const uint32_t kBluetoothRxPin = 12;
+static const uint32_t kBluetoothTxPin = 13;
+static const uint32_t kServoControlPin = 11;
+static const uint32_t kUltrasonicTriggerPin = 1;
+static const uint32_t kUltrasconicEchoPin = 0;
 
 // Servo motor positions
-#define SERVO_MIN_DEGREES 20
-#define SERVO_MAX_DEGREES 180
+static const uint32_t kServoMinDegrees = 20;
+static const uint32_t kServoMaxDegrees = 180;
 
 // Rotation step speed
-#define ROTATE_SPEED 0x1F
+static const uint32_t kRotateSpeed = 0x1F;
 
-#define MOTOR_STATE_COUNT 8  // Number of motor states in a cycle
+static const uint32_t kMotorStateCount = 8;  // Number of motor states in a cycle
 
 // Motor control takes up pins 2-9
-#define MOTOR_COUNT 2 // Number of motors
-#define MOTOR_PIN_COUNT 4  // Number of pins to control one motor
-#define RIGHT_MOTOR_START_NUMBER  2  // Start index of the left motor
-#define LEFT_MOTOR_START_NUMBER  6  // Start index of the right motor
-#define RIGHT_MOTOR_INDEX 0  // Index of right motor in motor_states
-#define LEFT_MOTOR_INDEX 1  // Index of left motor
+static const uint32_t kMotorCount = 2; // Number of motors
+static const uint32_t kMotorPinCount = 4;  // Number of pins to control one motor
+static const uint32_t kRightMotorStartNumber = 2;  // Start index of the left motor
+static const uint32_t kLeftMotorStartNumber = 6;  // Start index of the right motor
+static const uint32_t kRighMotorIndex = 0;  // Index of right motor in motor_states
+static const uint32_t kLeftMotorIndex = 1; // Index of left motor
 
 // Motor control mask information
-#define MOTOR_CONTROL_DIRECTION_MASK 0x80
-#define MOTOR_CONTROL_DIRECTION_INDEX 7
-#define MOTOR_CONTROL_VALUE_MASK 0x7F
+static const uint32_t kMotorControlDirectionMask = 0x80;
+static const uint32_t kMotorControlDirectionIndex = 7;
+static const uint32_t kMotorControlValueMask = 0x7F;
 
 // Receive buffer length for incoming bluetooth messages
-#define RECEIVE_BUFFER_LENGTH 16
+static const uint32_t kReceiveBufferLenght = 16;
 
 // Message IDs and sizes
-#define MOTOR_CONTROL_ID 1
-#define MOTOR_CONTROL_MESSAGE_SIZE 2
-#define SERVERO_CONTROL_ID 2
-#define SERVERO_CONTROL_MESSAGE_SIZE 1
-#define ULTRASONIC_REQUEST_ID 3
-#define ULTRASONIC_REPONSE_ID 4
-#define ULTRASONIC_RESPONSE_SIZE 4
-#define PING_REQUEST_ID 5
-#define PING_RESPONSE_ID 6
-#define MOVE_STRAIGHT_ID 7
-#define STOP_ID 8
-#define ROTATE_ID 9
-#define ROTATE_REQUEST_SIZE 2
+static const uint32_t kMotorControlId = 1;
+static const uint32_t kMotorControlMessageSize = 2;
+static const uint32_t kServoControlId = 2;
+static const uint32_t kServoControlMessageSize = 1;
+static const uint32_t kUltrasonicRequestId = 3;
+static const uint32_t kUltrasonicResponseId = 4;
+static const uint32_t kUltrasonicResponseSize = 4;
+static const uint32_t kPingRequestId = 5;
+static const uint32_t kPingResponseId = 6;
+static const uint32_t kMoveStraightId = 7;
+static const uint32_t kStopId = 8;
+static const uint32_t kRotateId = 9;
+static const uint32_t kRotateRequestSize = 2;
 
 // A state the motor bridge pins can be in
 struct MotorBridgeState {
@@ -73,7 +73,7 @@ struct MotorState {
 };
 
 // Each motor bridge state to cycle between in order to make the motor spin using half steps
-const MotorBridgeState motor_bridge_states[MOTOR_STATE_COUNT] {
+const MotorBridgeState motor_bridge_states[kMotorStateCount] {
   MotorBridgeState {.bridge_count = 0, .bridge_0 = 0,},
   MotorBridgeState {.bridge_count = 1, .bridge_0 = 0, .bridge_1 = 2},
   MotorBridgeState {.bridge_count = 0, .bridge_0 = 2,},
@@ -85,16 +85,16 @@ const MotorBridgeState motor_bridge_states[MOTOR_STATE_COUNT] {
 };
 
 // Define the bluetooth serial connection
-const SoftwareSerial BTserial(BLUETOOTH_RX_PIN, BLUETOOTH_TX_PIN);
+const SoftwareSerial BTserial(kBluetoothRxPin, kBluetoothTxPin);
 
 // Create the servo motor object
 const Servo servo_motor;
  
 // States for each motor, starts as set for no movement
-MotorState motor_states[MOTOR_COUNT] {
+MotorState motor_states[kMotorCount] {
   // Right Motor
   MotorState {  .reversed = 0,
-                .pin_0_index = RIGHT_MOTOR_START_NUMBER,
+                .pin_0_index = kRightMotorStartNumber,
                 .last_step_time = 0,
                 .step_delay = 0,
                 .direction = 0,
@@ -102,7 +102,7 @@ MotorState motor_states[MOTOR_COUNT] {
              },
   // Left Motor
   MotorState {  .reversed = 1,
-                .pin_0_index = LEFT_MOTOR_START_NUMBER,
+                .pin_0_index = kLeftMotorStartNumber,
                 .last_step_time = 0,
                 .step_delay = 0,
                 .direction = 0,
@@ -111,7 +111,7 @@ MotorState motor_states[MOTOR_COUNT] {
 };
 
 // Buffer for receiving bluetooth messages
-uint8_t receive_buffer[RECEIVE_BUFFER_LENGTH];
+uint8_t receive_buffer[kReceiveBufferLenght];
 
 // Amount of steps taken by the motor, used to record distance travled in a straight line and
 // to control how many steps are used in a turn operation
@@ -123,13 +123,13 @@ uint32_t steps_taken = 0;
 // motor_state_index: The index of the motor_state the delay is being set for
 void setMotorDelayAndDirection(uint8_t control_value, uint8_t motor_state_index) {
   // Get the direction and speed value from the control value
-  uint8_t direction = (control_value & MOTOR_CONTROL_DIRECTION_MASK) >> MOTOR_CONTROL_DIRECTION_INDEX;
-  unsigned long value = (unsigned long) control_value & MOTOR_CONTROL_VALUE_MASK; 
+  uint8_t direction = (control_value & kMotorControlDirectionMask) >> kMotorControlDirectionIndex;
+  unsigned long value = (unsigned long) control_value & kMotorControlValueMask;
 
   // Calculate the delay, if the speed value is 0 then delay should also be zero
   unsigned long delay = 0;
   if (value > 0) {
-    delay = MAX_STEP_DELAY - ((value * (MAX_STEP_DELAY - MIN_STEP_DELAY)) / MOTOR_CONTROL_VALUE_MASK);
+    delay = kMaxStepDelay - ((value * (kMaxStepDelay - kMinStepDelay)) / kMotorControlValueMask);
   }
 
   // Set the motor states
@@ -141,7 +141,7 @@ void setMotorDelayAndDirection(uint8_t control_value, uint8_t motor_state_index)
 void stepMotorsIfTime() {
   unsigned long current_time = micros();
   bool step_taken = false;
-  for (uint8_t index = 0; index < MOTOR_COUNT; index++) {
+  for (uint8_t index = 0; index < kMotorCount; index++) {
     MotorState *motor_state = &motor_states[index];
 
     // If delay is set to zero then the motor should not be stepped
@@ -160,12 +160,12 @@ void stepMotorsIfTime() {
     int8_t incr = (motor_state->reversed == motor_state->direction) ? 1 : -1;
     motor_state->bridge_state_index += incr;
     if (motor_state->bridge_state_index < 0) {
-      motor_state->bridge_state_index = MOTOR_STATE_COUNT - 1;
+      motor_state->bridge_state_index = kMotorStateCount - 1;
     }
-    motor_state->bridge_state_index %= MOTOR_STATE_COUNT;
+    motor_state->bridge_state_index %= kMotorStateCount;
 
     // Turn all motor pins off
-    for (uint8_t pin_offset = 0; pin_offset < (MOTOR_PIN_COUNT); pin_offset++) {
+    for (uint8_t pin_offset = 0; pin_offset < (kMotorPinCount); pin_offset++) {
       digitalWrite(pin_offset + motor_state->pin_0_index, LOW);
     }
 
@@ -195,21 +195,21 @@ void rotate(uint8_t direction, uint8_t steps) {
   steps_taken = 0;
 
   // Set the motor states for rotation
-  uint8_t left_motor_speed = ROTATE_SPEED;
-  uint8_t right_motor_speed = ROTATE_SPEED;
+  uint8_t left_motor_speed = kRotateSpeed;
+  uint8_t right_motor_speed = kRotateSpeed;
 
   // Determin which motor should be reversed
   if (direction == 0) {
-    left_motor_speed |= MOTOR_CONTROL_DIRECTION_MASK;
+    left_motor_speed |= kMotorControlDirectionMask;
   } else if (direction == 1) {
-    right_motor_speed |= MOTOR_CONTROL_DIRECTION_MASK;
+    right_motor_speed |= kMotorControlDirectionMask;
   } else {
     return;
   }
 
   // Set the motor states
-  setMotorDelayAndDirection(right_motor_speed, RIGHT_MOTOR_INDEX);
-  setMotorDelayAndDirection(left_motor_speed, LEFT_MOTOR_INDEX);
+  setMotorDelayAndDirection(right_motor_speed, kRighMotorIndex);
+  setMotorDelayAndDirection(left_motor_speed, kLeftMotorIndex);
 
   // Allow the motors to complete each step
   while (steps_taken != static_cast<uint32_t>(steps)) {
@@ -217,8 +217,8 @@ void rotate(uint8_t direction, uint8_t steps) {
   }
 
   // Stop the motors
-  setMotorDelayAndDirection(0, RIGHT_MOTOR_INDEX);
-  setMotorDelayAndDirection(0, LEFT_MOTOR_INDEX);
+  setMotorDelayAndDirection(0, kRighMotorIndex);
+  setMotorDelayAndDirection(0, kLeftMotorIndex);
 }
 
 /**
@@ -227,11 +227,11 @@ void rotate(uint8_t direction, uint8_t steps) {
  */
 void setServoPos(uint8_t degrees_byte) {
   int8_t servo_degrees = static_cast<int16_t>(degrees_byte);
-  int16_t servo_center = (SERVO_MAX_DEGREES + SERVO_MIN_DEGREES) / 2;
+  int16_t servo_center = (kServoMaxDegrees + kServoMinDegrees) / 2;
   int16_t translated_degrees = static_cast<int16_t>(servo_center + servo_degrees);
 
   // Set the motor position if the position is valid
-  if ((translated_degrees < SERVO_MIN_DEGREES) || (translated_degrees > SERVO_MAX_DEGREES)) {
+  if ((translated_degrees < kServoMinDegrees) || (translated_degrees > kServoMaxDegrees)) {
     return;
   }
   servo_motor.write(translated_degrees);
@@ -242,10 +242,10 @@ void setServoPos(uint8_t degrees_byte) {
  */
 uint32_t getUltrasonicPulse() {
   // Pulse trigger pin
-  digitalWrite(TRIGGER_PIN, HIGH);
+  digitalWrite(kUltrasonicTriggerPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN, LOW);
-  uint32_t duration = pulseIn(ECHO_PIN, HIGH);
+  digitalWrite(kUltrasonicTriggerPin, LOW);
+  uint32_t duration = pulseIn(kUltrasconicEchoPin, HIGH);
   return duration;
 }
 
@@ -275,9 +275,9 @@ void sendBluetoothMessage(uint8_t *buffer, int size, uint8_t message_id) {
  * Send the UltraSonicResponse with the duration
  */
 void sendUltraSonicResponse(uint32_t duration) {
-  uint8_t buffer[ULTRASONIC_RESPONSE_SIZE];
+  uint8_t buffer[kUltrasonicResponseSize];
   memcpy(buffer, &duration, sizeof(duration));
-  sendBluetoothMessage(buffer, ULTRASONIC_RESPONSE_SIZE, ULTRASONIC_REPONSE_ID);
+  sendBluetoothMessage(buffer, kUltrasonicResponseSize, kUltrasonicResponseId);
 }
 
 // Process a bluetooth message if one is available
@@ -291,49 +291,49 @@ void processBluetoothMessageIfAvailable() {
     uint8_t message_id = BTserial.read();
 
     // If a ping request
-    if (message_id == PING_REQUEST_ID) {
-      sendBluetoothMessage(nullptr, 0, PING_RESPONSE_ID);
+    if (message_id == kPingRequestId) {
+      sendBluetoothMessage(nullptr, 0, kPingResponseId);
     }
 
     // If a motor control message
-    else if (message_id == MOTOR_CONTROL_ID) {
-      readBytesIntoReceiveBuffer(MOTOR_CONTROL_MESSAGE_SIZE);
+    else if (message_id == kMotorControlId) {
+      readBytesIntoReceiveBuffer(kMotorControlMessageSize);
       // Set motor speed from control bytes
-      setMotorDelayAndDirection(receive_buffer[0], RIGHT_MOTOR_INDEX);
-      setMotorDelayAndDirection(receive_buffer[1], LEFT_MOTOR_INDEX);
+      setMotorDelayAndDirection(receive_buffer[0], kRighMotorIndex);
+      setMotorDelayAndDirection(receive_buffer[1], kLeftMotorIndex);
     }
 
     // If a servo control message
-    else if (message_id == SERVERO_CONTROL_ID) {
-      readBytesIntoReceiveBuffer(SERVERO_CONTROL_MESSAGE_SIZE);
+    else if (message_id == kServoControlId) {
+      readBytesIntoReceiveBuffer(kServoControlMessageSize);
       // set servo position
       setServoPos(receive_buffer[0]);
     }
 
     // If a ultrasonic request
-    else if (message_id == ULTRASONIC_REQUEST_ID) {
+    else if (message_id == kUltrasonicRequestId) {
       sendUltraSonicResponse(getUltrasonicPulse());
     }
 
     // If this is a move straight request
-    else if (message_id == MOVE_STRAIGHT_ID) {
+    else if (message_id == kMoveStraightId) {
       // Set both motors to move foward and reset steps taken
-      setMotorDelayAndDirection(0x7F, RIGHT_MOTOR_INDEX);
-      setMotorDelayAndDirection(0x7F, LEFT_MOTOR_INDEX);
+      setMotorDelayAndDirection(0x7F, kRighMotorIndex);
+      setMotorDelayAndDirection(0x7F, kLeftMotorIndex);
       steps_taken = 0;
     }
 
     // If this is a rotate request
-    else if (message_id == ROTATE_ID) {
-      readBytesIntoReceiveBuffer(ROTATE_REQUEST_SIZE);
+    else if (message_id == kRotateId) {
+      readBytesIntoReceiveBuffer(kRotateRequestSize);
       rotate(receive_buffer[0], receive_buffer[1]);
       // TODO(mwallberg): send the motor rotate response
     }
 
     // If this is a stop request
-    else if (message_id == STOP_ID) {
-      setMotorDelayAndDirection(0, RIGHT_MOTOR_INDEX);
-      setMotorDelayAndDirection(0, LEFT_MOTOR_INDEX);
+    else if (message_id == kStopId) {
+      setMotorDelayAndDirection(0, kRighMotorIndex);
+      setMotorDelayAndDirection(0, kLeftMotorIndex);
       // TODO(mwallberg): send the steps taken to the PI
     }
 }
@@ -342,7 +342,7 @@ void processBluetoothMessageIfAvailable() {
 void setup()
 {
   // Set the motor pins to output
-  for(int pin = RIGHT_MOTOR_START_NUMBER; pin < (LEFT_MOTOR_START_NUMBER + MOTOR_PIN_COUNT); pin++ ) {
+  for(int pin = kRightMotorStartNumber; pin < (kLeftMotorStartNumber + kMotorPinCount); pin++ ) {
     pinMode(pin, OUTPUT);
   }
   // Init bluetooth serial connection
@@ -350,13 +350,13 @@ void setup()
   BTserial.listen();
 
   // Attach the servo motor and center it
-  servo_motor.attach(SERVO_CONTROL_PIN);
-  servo_motor.write((SERVO_MAX_DEGREES + SERVO_MIN_DEGREES) / 2);
+  servo_motor.attach(kServoControlPin);
+  servo_motor.write((kServoMaxDegrees + kServoMinDegrees) / 2);
 
   // Set up ultrasonic pins
-  pinMode(TRIGGER_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  digitalWrite(TRIGGER_PIN, LOW);
+  pinMode(kUltrasonicTriggerPin, OUTPUT);
+  pinMode(kUltrasconicEchoPin, INPUT);
+  digitalWrite(kUltrasonicTriggerPin, LOW);
 }
 
 // Control loop, processes messages and runs motors
